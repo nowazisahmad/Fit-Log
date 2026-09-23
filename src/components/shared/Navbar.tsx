482488c9bc75764bc981e0255d3316b6c81bc18c
Nowazis Ahmad Usama <1289rolex@gmail.com>
2026-09-23 23:37:56 +0600

    Navbar added

---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,63 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/assets/logo.png";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost md:hidden lg:hidden">
+            <svg
+              aria-label="Menu"
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex={-1}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <Link href="/">Workout</Link>
+            </li>
+            <li>
+              <Link href="/">My Plan</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="flex gap-2 items-center">
+          <Image src={logo} alt="Fit Log Logo" />
+          <span className="text-green-300 text-4xl font-bold ">FITLOG</span>
+        </div>
+      </div>
+      <div className="navbar-center hidden md:flex lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link href="/">Workout</Link>
+          </li>
+          <li>
+            <Link href="/">My Plan</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end gap-2">
+        <button className="btn btn-accent">Plan</button>
+        <button className="btn btn-success">Saved</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
