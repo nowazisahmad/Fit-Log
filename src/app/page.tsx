b13f9f112e2117063f302925117c61484e18be8c
Nowazis Ahmad Usama <1289rolex@gmail.com>
2026-09-24 05:40:58 +0600

    Fitlog added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Banner from "@/components/homepage/Banner";
+import Fitlog from "@/components/homepage/Fitlog";
 
 export default function Home() {
   return (
     <div className="container mx-auto py-4">
       <Banner/>
+      <Fitlog/>
     </div>
   );
 }
